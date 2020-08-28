f8582bb7c141377a8ccda2a7ffc3fc32805e7704
Xiao Liang <xiliang@redhat.com>
2020-08-28 16:01:06 +0800

    added 8 cloud-init cases
    - test_check_cloudinit_ds_identify_found
    - test_check_cloudinit_log_imdsv2
    - test_check_cloudinit_log_unexpected
    - test_check_cloudinit_log_critical
    - test_check_cloudinit_log_warn
    - test_check_cloudinit_log_error
    - test_check_metadata
    - test_check_output_isexist
    
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="os-tests",
-    version="0.0.3",
+    version="0.0.4",
     author="Xiao Liang",
     author_email="xiliang@redhat.com",
     description="Lightweight, fast check and tests collection for Linux OS",
