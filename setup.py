cd60e464b452673434499ee1ca20a3cc7ccae89c
Xiao Liang <xiliang@redhat.com>
2020-09-27 10:15:34 +0800

    bump to v0.0.13 for trigger test
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="os-tests",
-    version="0.0.12",
+    version="0.0.13",
     author="Xiao Liang",
     author_email="xiliang@redhat.com",
     description="Lightweight, fast check and tests collection for Linux OS",
