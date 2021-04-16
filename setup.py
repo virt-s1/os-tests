4d99b73c5e798adb6e46495b51edbc93fddbbb87
Xiao Liang <xiliang@redhat.com>
2021-04-16 16:25:11 +0800

    bump to v0.0.32
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="os-tests",
-    version="0.0.31",
+    version="0.0.32",
     author="Xiao Liang",
     author_email="xiliang@redhat.com",
     description="Lightweight, fast check and tests collection for Linux OS",
