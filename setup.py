2936e8c08f20d52fe68010205bb42cba690ee165
Xiao Liang <xiliang@redhat.com>
2022-02-22 15:16:34 +0800

    move project to virt-s1
    2 more changes:
    - specify markupsafe version to 1.1.1 because 2.1.0 dropped soft_unicode
      that jinjia2 used.
      Upgrade both jinjia2 and markupsafe to the latest also works, but rhel9.0 has 1.1.1
      by default.
    - add a action job to notify gchat os-tests space
    
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Lightweight, fast and customer centric tests collection for Linux OS.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/liangxiao1/os-tests",
+    url="https://github.com/virt-s1/os-tests",
     #packages=setuptools.find_packages(),
     packages=[ 'os_tests', 'os_tests.tests', 'os_tests.libs'],
     package_data={
@@ -22,7 +22,7 @@ setuptools.setup(
     },
     include_package_data=True,
     #data_files=[('/'+os.path.expanduser("~"), ['cfg/os-tests.yaml']),],
-    install_requires=['PyYAML', 'Jinja2<=2.11.3', 'tipset'],
+    install_requires=['PyYAML', 'Jinja2<=2.11.3', 'tipset>=0.0.15', 'markupsafe<=1.1.1' ],
     license="GPLv3+",
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
