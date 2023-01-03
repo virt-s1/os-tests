706c3e2a3269d60c4977928f6c9b3a579df777c9
Wei Shi <wshi@redhat.com>
2023-01-03 15:55:16 +0800

    Add clocksource for ppc64le
    Signed-off-by: Wei Shi <wshi@redhat.com>

--- os_tests/tests/test_general_check.py
+++ os_tests/tests/test_general_check.py
@@ -143,6 +143,7 @@ class TestGeneralCheck(unittest.TestCase):
             the clocksource matched to the system is expected.
             Xen - xen,tsc,hpet,acpi_pm,
             aarch64 - arch_sys_counter,
+            ppc64le - timebase,
             Microsoft - hyperv_clocksource_tsc_page,acpi_pm,
             AuthenticAMD - kvm-clock,tsc,acpi_pm,
             GenuineIntel - kvm-clock,tsc,acpi_pm
@@ -154,6 +155,8 @@ class TestGeneralCheck(unittest.TestCase):
             expect_clocks = 'xen,tsc,hpet,acpi_pm'
         elif 'aarch64' in output:
             expect_clocks = 'arch_sys_counter'
+        elif 'ppc64le' in output and 'KVM' in output and not utils_lib.is_metal(self):
+            expect_clocks = 'timebase'
         elif 'Microsoft' in output:
             expect_clocks = 'hyperv_clocksource_tsc_page,acpi_pm'
         elif 'AuthenticAMD' in output and 'KVM' in output and not utils_lib.is_metal(self):
