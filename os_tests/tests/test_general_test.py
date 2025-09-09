efdeea39df5fb0836fc99048340e54e44416b9ea
Li Tian <litian@redhat.com>
2025-09-09 12:34:56 +0800

    utils_lib: Fix subscription-manager status return status
    The output message of 'subscription-manager status' has changed.
    Using just the return code to determine register status.
    
    Fixes: 2b38965a614d3 ("Add rhsm is_rhsm_registered, enable_auto_registration, rhsm_register, rhsm_unregister")
    Signed-off-by: Li Tian <litian@redhat.com>

--- os_tests/tests/test_general_test.py
+++ os_tests/tests/test_general_test.py
@@ -532,9 +532,7 @@ int main(int argc, char *argv[])
             out = utils_lib.run_cmd(self, cmd, msg='try to check subscription identity')
             cmd = "sudo subscription-manager list --installed"
             out = utils_lib.run_cmd(self, cmd, msg='try to list currently installed on the system')
-            cmd = "sudo subscription-manager status"
-            status = utils_lib.run_cmd(self, cmd, msg='try to check subscription status', ret_status=True, ret_out=False)
-            if status == 0:
+            if utils_lib.is_rhsm_registered(self, cancel_case=True):
                 self.log.info("auto subscription registered completed")
                 cmd = "sudo insights-client --register"
                 utils_lib.run_cmd(self, cmd, msg='check if insights-client can register successfully')
